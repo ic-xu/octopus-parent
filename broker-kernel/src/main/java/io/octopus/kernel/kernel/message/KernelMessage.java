9bbcfaf99730f2f3907d857f9981e304e9850665
陈旭 <chenxu9741@cvte.com>
2022-07-15 19:57:06 +0800

    feat(v1.2.0): 新增 qos1 和 部分 qos2 消息分发

---
+++ broker-kernel/src/main/java/io/octopus/kernel/kernel/message/KernelMessage.java
@@ -0,0 +1,61 @@
+package io.octopus.kernel.kernel.message;
+import java.util.Objects;
+
+/**
+ * @author chenxu
+ * @version 1
+ * @date 2022/1/5 8:14 下午
+ */
+public class KernelMessage {
+
+    private final short packageId;
+
+
+
+    private final PubEnum pubEnum;
+
+    public KernelMessage(short packageId, PubEnum pubEnum) {
+        this.packageId = packageId;
+        this.pubEnum = pubEnum;
+    }
+
+
+
+
+    public PubEnum getPubEnum() {
+        return pubEnum;
+    }
+
+
+
+
+
+
+
+    public Short packageId() {
+        return packageId;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof KernelPayloadMessage)) {
+            return false;
+        }
+        KernelMessage kernelMsg = (KernelMessage) o;
+        return Objects.equals(packageId(), kernelMsg.packageId())
+                && Objects.equals(kernelMsg.pubEnum,this.pubEnum);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(packageId());
+    }
+
+
+
+}
